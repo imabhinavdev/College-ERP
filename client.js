ae109666ab9a178ec1497585ca9baf1178b6c3b1
Abhinav Singh <abhinavas430@gmail.com>
2023-10-19 23:02:47 +0530

    Fixing URL

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABSE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 
 module.exports = supabase;
