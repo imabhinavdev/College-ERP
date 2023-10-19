3d2b0cb76800c80b83c863d34e066aecb08b3c43
Abhinav Singh <abhinavas430@gmail.com>
2023-10-19 22:57:06 +0530

    Keeping things private

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 // fetchdata.js
+require('dotenv').config();
+
 const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://wfujuyvevbanorbcibfe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmdWp1eXZldmJhbm9yYmNpYmZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTgxNzc2NCwiZXhwIjoyMDA3MzkzNzY0fQ.n07wso6YeNvelHsPts4h-5gMeOUXy9ydDv4v3JoGanA');
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABSE_KEY);
 
 
 module.exports = supabase;
