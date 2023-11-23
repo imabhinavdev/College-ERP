c9869df2c2df42f37d90057b6d637824a1b75ad7
Abhinav Singh <abhinavas430@gmail.com>
2023-11-24 01:11:25 +0530

    Vercel 500 Fix

--- client.js
+++
@@ -1,8 +0,0 @@
-// fetchdata.js
-require('dotenv').config();
-
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
-
-module.exports = supabase;
